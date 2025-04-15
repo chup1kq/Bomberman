a2982319acba7a32ee799b5a5afcb7da8416f7e2
Zinovev Ivan <izinovev77@gmail.com>
2025-04-15 20:16:54 +0300

    Position

---
+++ src/model/geometry/Position.java
@@ -0,0 +1,46 @@
+package model.geometry;
+
+public class Position {
+
+    private double _x;
+
+    private double _y;
+
+    public Position(double x, double y) {
+        _x = x;
+        _y = y;
+    }
+
+    public double getX() {
+        return _x;
+    }
+
+    public double getY() {
+        return _y;
+    }
+
+    public void setX(double x) {
+        _x = x;
+    }
+
+    public void setY(double y) {
+        _y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null)
+            return false;
+
+        if (!(o instanceof Position position))
+            return false;
+
+        return Double.compare(_x, position._x) == 0 &&
+                Double.compare(_y, position._y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * Double.hashCode(_x) + Double.hashCode(_y);
+    }
+}
